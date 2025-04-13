72c97a5d4ef4e6a0fc375bab0b5093042df0dc25
timunie <tim.uniewski@gmail.com>
2025-04-13 18:20:19 +0200

    slnx is still bugged, so reverting this for now
    It would have been nice but not worth it.

--- build.ps1
+++ build.ps1
@@ -28,13 +28,39 @@ $env:SHFBRoot = ".\src\packages\ewsoftware.shfb\2025.3.22\tools\"
 
 # Define a list of dotNET projects to build
 $projectsToBuild = @(
-    # Build Avalonia dependencies
-	"ext/Avalonia/src/Markup/Avalonia.Markup.Xaml/Avalonia.Markup.Xaml.csproj",
-	"ext/Avalonia/src/tools/Avalonia.Generators/Avalonia.Generators.csproj",
-	"ext/Avalonia/src/Avalonia.Build.Tasks/Avalonia.Build.Tasks.csproj",
-	
-    # Build all documentation sources. Check the slnx file to add or remove additional projects to use.
-	"src/ApiDocumentation/DocumentationSources.slnx",
+    # Documented Avalonia projects
+	"ext/Avalonia/packages/Avalonia/Avalonia.csproj", 
+	"ext/Avalonia/src/tools/Avalonia.Analyzers/Avalonia.Analyzers.csproj", 
+	"ext/Avalonia/src/Android/Avalonia.Android/Avalonia.Android.csproj", 
+	"ext/Avalonia/src/Avalonia.Base/Avalonia.Base.csproj", 
+	"ext/Avalonia/src/Browser/Avalonia.Browser/Avalonia.Browser.csproj", 
+	"ext/Avalonia/src/Browser/Avalonia.Browser.Blazor/Avalonia.Browser.Blazor.csproj", 
+	"ext/Avalonia/src/Avalonia.Controls/Avalonia.Controls.csproj", 
+	"ext/Avalonia/src/Avalonia.Controls.ColorPicker/Avalonia.Controls.ColorPicker.csproj", 
+	"ext/Avalonia/src/Avalonia.Controls.DataGrid/Avalonia.Controls.DataGrid.csproj", 
+	"ext/Avalonia/src/Avalonia.Desktop/Avalonia.Desktop.csproj", 
+	"ext/Avalonia/src/Avalonia.Diagnostics/Avalonia.Diagnostics.csproj", 
+	"ext/Avalonia/src/Avalonia.Dialogs/Avalonia.Dialogs.csproj", 
+	"ext/Avalonia/src/Windows/Avalonia.Direct2D1/Avalonia.Direct2D1.csproj", 
+	"ext/Avalonia/src/Avalonia.Fonts.Inter/Avalonia.Fonts.Inter.csproj", 
+	"ext/Avalonia/src/Avalonia.FreeDesktop/Avalonia.FreeDesktop.csproj", 
+	"ext/Avalonia/src/iOS/Avalonia.iOS/Avalonia.iOS.csproj", 
+	"ext/Avalonia/src/Linux/Avalonia.LinuxFramebuffer/Avalonia.LinuxFramebuffer.csproj", 
+	"ext/Avalonia/src/Markup/Avalonia.Markup/Avalonia.Markup.csproj", 
+	"ext/Avalonia/src/Markup/Avalonia.Markup.Xaml/Avalonia.Markup.Xaml.csproj", 
+	"ext/Avalonia/src/Avalonia.Metal/Avalonia.Metal.csproj", 
+	"ext/Avalonia/src/Avalonia.MicroCom/Avalonia.MicroCom.csproj", 
+	"ext/Avalonia/src/Avalonia.Native/Avalonia.Native.csproj", 
+	"ext/Avalonia/src/Avalonia.OpenGL/Avalonia.OpenGL.csproj", 
+	"ext/Avalonia/src/Avalonia.ReactiveUI/Avalonia.ReactiveUI.csproj", 
+	"ext/Avalonia/src/Skia/Avalonia.Skia/Avalonia.Skia.csproj", 
+	"ext/Avalonia/src/Avalonia.Themes.Fluent/Avalonia.Themes.Fluent.csproj", 
+	"ext/Avalonia/src/Avalonia.Themes.Simple/Avalonia.Themes.Simple.csproj", 
+	"ext/Avalonia/src/Avalonia.Vulkan/Avalonia.Vulkan.csproj", 
+	"ext/Avalonia/src/Windows/Avalonia.Win32/Avalonia.Win32.csproj", 
+	"ext/Avalonia/src/Windows/Avalonia.Win32.Automation/Avalonia.Win32.Automation.csproj", 
+	"ext/Avalonia/src/Windows/Avalonia.Win32.Interoperability/Avalonia.Win32.Interoperability.csproj", 
+	"ext/Avalonia/src/Avalonia.X11/Avalonia.X11.csproj", 
 	
 	# Sandcastle documentation
 	"src/DocusaurusExportPlugin/DocusaurusExportPlugin.csproj",
