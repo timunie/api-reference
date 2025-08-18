7a2ea8cab0c408c64fb3b3ef9f36956acffb65b4
Tim <47110241+timunie@users.noreply.github.com>
2025-08-18 21:53:28 +0200

    some test

--- build.ps1
+++ build.ps1
@@ -12,9 +12,8 @@ param (
 $version = Get-Content ./AvaloniaVersion.txt
 
 # Update git submodules. 
+git submodule update --init --remote --depth=1 -f
 git submodule update --init --recursive
-git submodule update --remote --depth = 1
-git submodule update --recursive
 
 # git submodule foreach -q --recursive 'branch="$(git config -f $toplevel.gitmodules submodule.$name.branch)"; git checkout $branch'
 
